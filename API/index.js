79feb9b18fbad377071ca26e074884d0a598db66
Batsirai-sudo <batsybbay@gmail.com>
2021-03-20 03:45:12 +0200

    edited index file

--- API/index.js
+++ API/index.js
@@ -1,2 +1,10 @@
+const express = require('express');
+const router = express.Router();
 
+router.get('/batsiraiferhatpay/good', (req, res, next) => {
+	console.log('logged batsy');
 
+	res.send('received');
+});
+
+module.exports = router;
