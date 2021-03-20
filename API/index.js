24e74638104829e3f4486707947bc191b14860a2
Batsirai-sudo <batsybbay@gmail.com>
2021-03-20 15:39:15 +0200

    Updated app.js with the actually files

--- API/index.js
+++ API/index.js
@@ -1,5 +1,13 @@
-const otpMobile = require('./OTPMobileNumber');
+const express = require('express');
+const router = express.Router();
 
-module.exports = {
-	otpMobile,
-};
+router.get('/batsiraiferhatpay/good', (req, res, next) => {
+	console.log('logged batsy');
+
+	res.send({
+		result: 'Sent Received',
+		otp: 'OTP.token',
+	});
+});
+
+module.exports = router;
