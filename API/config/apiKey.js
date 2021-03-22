24ef7ebb9a0d9e870cb78554cba86f110575ffe8
Batsirai-sudo <batsybbay@gmail.com>
2021-03-22 03:36:56 +0200

    added a config folder45

--- API/config/apiKey.js
+++ API/config/apiKey.js
@@ -1,8 +1,8 @@
 const queries = require('./queries');
 
 const getApiType = (type, conn) => {
-	// const [rows] = conn.execute(queries.getApi);
-	console.log('---------------------------------', conn, 'rowsrowsrowsrowsrowsrows', type);
+	const [rows] = conn.query(queries.getApi);
+	console.log('---------------------------------', rows, 'rowsrowsrowsrowsrowsrows', type);
 
 	// const filter = rows.map((x) => x.type === type);
 	// console.log('getApiTypegetApiType', filter);
