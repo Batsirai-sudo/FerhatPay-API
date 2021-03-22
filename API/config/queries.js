93cc0ae99c22022e8ec0e3e8a3e3f5b03b745cfc
Batsirai-sudo <batsybbay@gmail.com>
2021-03-22 23:53:52 +0200

    Updated passport js file

--- API/config/queries.js
+++ API/config/queries.js
@@ -6,8 +6,8 @@ module.exports = {
 	createAccount: 'INSERT INTO account SET ?',
 	incrementRegistration: 'UPDATE statistics SET ? WHERE id = 1',
 	currentRegistrationCount: 'SELECT * FROM statistics',
-	getUserByAccountNumber: 'SELECT  users.*, account.AccountNumber, account.account_id FROM account JOIN users ON account.UserID=users.id WHERE account.AccountNumber = ?';
-	getUserByID: 'SELECT users.*,account.AccountNumber, account.account_id FROM users JOIN account ON users.id = account.UserID where users.id = '
-
+	getUserByAccountNumber:
+		'SELECT  users.*, account.AccountNumber, account.account_id FROM account JOIN users ON account.UserID=users.id WHERE account.AccountNumber = ?',
+	getUserByID:
+		'SELECT users.*,account.AccountNumber, account.account_id FROM users JOIN account ON users.id = account.UserID where users.id =? ',
 };
-	
